6b9f25faf31e320bfd3afcc83109f403d7058c8d
JeremyWallace1 <jeremy.wallace@telus.com>
2022-11-25 17:00:34 -0700

    fixed formatting of function and made recursive so it can handle multiple levels of arrays within arrays. passes all tests.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function sumItems(array) {
+const sumItems = function(array) {
+  let sum = 0;
   // Sum all the numbers in the array
-}
-
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
+};
+//console.log(sumItems([1, 2, 3, 4, 5]));
 module.exports = sumItems;
